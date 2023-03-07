51737b4ad767f3ad27db33cfc4a01890e4e6f7a5
MrValk <n.s.peters05@gmail.com>
2023-03-07 16:34:58 +0100

    Added link to database structure view on DrawSQL

--- src-tauri/src/database/models.rs
+++ src-tauri/src/database/models.rs
@@ -9,6 +9,9 @@ pub mod mods;
 
 pub mod settings;
 
+// Organized view of the database structure
+// https://drawsql.app/teams/egel-developers/diagrams/main-2
+
 pub trait DbModel {
     fn save(&self, db: &Connection) -> Result<(), Box<dyn Error>>;
 }
